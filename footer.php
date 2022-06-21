1560b6f0df713c09abdcd994d26b3ec92be103d4
Thies <Thiesvanderzon@gmail.com>
2022-06-21 11:35:01 +0200

    tussen commit = aanpassingen gemaakt die aangeven zijn door Noeël (zie trello)

--- footer.php
+++ footer.php
@@ -2,11 +2,25 @@
 <?php wp_footer() ?>
 
 <footer>
-    <!-- <?php
-    wp_nav_menu( array( 'theme_location' => 'footer-menu' ) );
-    ?> -->
-
-    footer
+    <div class="content-container">
+        <div class="nav-title">
+            <?php dynamic_sidebar( 'footer_area_one' ); ?>
+        </div>
+        <div class="links-flex-div">
+            <div class="nav-links">
+                <?php dynamic_sidebar( 'footer_area_two' ); ?>
+            </div>
+            <div class="social-links">
+                <?php dynamic_sidebar( 'footer_area_three' ); ?>
+            </div>
+        </div>
+        <div class="hr">
+            <?php dynamic_sidebar( 'footer_area_four' ); ?>
+        </div>
+        <div class="madeby-div">
+            <?php dynamic_sidebar( 'footer_area_five' ); ?>
+        </div>
+    </div>
 </footer>
 </body>
 </html>
--- functions.php
+++ functions.php
@@ -21,5 +21,62 @@ function register_my_menus() {
    );
  }
  add_action( 'init', 'register_my_menus' );
+
+ function register_widget_areas() {
+
+  register_sidebar( array(
+      'name'          => 'Titel footer',
+      'id'            => 'footer_area_one',
+      'description'   => 'This widget area discription',
+      'before_widget' => '<section class="footer-area footer-area-one">',
+      'after_widget'  => '</section>',
+      'before_title'  => '<h4 class="footer-text-one">',
+      'after_title'   => '</h4>',
+  ));
+
+  register_sidebar( array(
+      'name'          => 'Navbar footer',
+      'id'            => 'footer_area_two',
+      'description'   => 'This widget area discription',
+      'before_widget' => '<section class="footer-area footer-area-two">',
+      'after_widget'  => '</section>',
+      'before_title'  => '<h4>',
+      'after_title'   => '</h4>',
+  ));
+
+  register_sidebar( array(
+      'name'          => 'Social links',
+      'id'            => 'footer_area_three',
+      'description'   => 'This widget area discription',
+      'before_widget' => '<section class="footer-area footer-area-three">',
+      'after_widget'  => '</section>',
+      'before_title'  => '<h4>',
+      'after_title'   => '</h4>',
+  ));
+
+  register_sidebar( array(
+      'name'          => 'Lange streep',
+      'id'            => 'footer_area_four',
+      'description'   => 'This widget area discription',
+      'before_widget' => '<section class="footer-area footer-area-four">',
+      'after_widget'  => '</section>',
+      'before_title'  => '<h4>',
+      'after_title'   => '</h4>',
+  ));
+
+  register_sidebar( array(
+      'name'          => 'Made by ""',
+      'id'            => 'footer_area_five',
+      'description'   => 'This widget area discription',
+      'before_widget' => '<section class="footer-area footer-area-five">',
+      'after_widget'  => '</section>',
+      'before_title'  => '<h4>',
+      'after_title'   => '</h4>',
+  ));
+
+}
+
+add_action( 'widgets_init', 'register_widget_areas' );
 ?>
+
 
