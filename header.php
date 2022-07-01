1c64266750d7121e62d09f07c06c24aeaeeca58a
Thies <Thiesvanderzon@gmail.com>
2022-07-01 14:29:21 +0200

    viewport mobile

--- header.php
+++ header.php
@@ -2,6 +2,7 @@
 <html <?php language_attributes(); ?>>
 
 <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="<?php bloginfo('charset'); ?>">
     <title><?php bloginfo('name'); ?></title>
     <?php wp_head() ?>
